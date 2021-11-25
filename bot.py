31f4b0e040faf6d7231444a3565020ce8c596741
Terminator <fresh-ter@yandex.com>
2021-11-25 21:27:28 +0400

    #

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ p = Penger(token = tS.token)
 def start_command(self):
 	print(self.data)
 
-	if self.data['text'] = "/start":
+	if self.data['text'] == "/start":
 		p.sendMessageToChat(self.data, "Hello :-)")
 	else:
 		command_args = self.data['text'].split()
 		if len(command_args) > 1:
 			arg = str(command_args[1])
 
-			if arg[:4] == "task"
+			if arg[:4] == "task":
 				Task.getTaskFromStart(start_message_argument=arg)
 
 
