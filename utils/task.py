6b188b6862582ef07df01c7ed6441d128762ac14
Terminator <fresh-ter@yandex.com>
2021-11-28 02:01:10 +0400

    Update task.py

--- utils/task.py
+++ utils/task.py
@@ -6,7 +6,6 @@ class Task:
 		message = start_message_argument
 		print("Parsing start message: ", message)
 
-		# task_1
 		if re.match("^task_\\d{,3}$", message):
 			task_number = message[5:]
 			if task_number.isdigit():
