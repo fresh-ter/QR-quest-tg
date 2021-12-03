adeb9244600914f8719cfa9f1d681f01500e49bb
Terminator <fresh-ter@yandex.com>
2021-12-03 23:51:10 +0400

    Cleared run.py from test-code

--- run.py
+++ run.py
@@ -6,19 +6,6 @@ def run_bot(usersDB, tasksDB, solutionsDB):
 	bot.main(usersDB, tasksDB, solutionsDB)
 
 
-def create_test_users(usersDB):
-	usersDB.createAndAddNewUser('123', 'qwerty')
-	usersDB.createAndAddNewUser('124')
-	usersDB.createAndAddNewUser('125', 'test')
-	usersDB.createAndAddNewUser('126', 'Steve')
-
-def create_test_tasks(tasksDB):
-	tasksDB.createAndAddNewTask("What is it?", "Linux", 1000)
-	tasksDB.createAndAddNewTask("Who is it?", "Tux", 700)
-	tasksDB.createAndAddNewTask("Where is it?", "Antarctica", 750)
-	tasksDB.createAndAddNewTask("When is it?", "Now", 5000)
-
-
 def main():
 	usersDB = UsersDB()
 	usersDB.start()
@@ -28,30 +15,6 @@ def main():
 
 	solutionsDB = SolutionsDB()
 	solutionsDB.start()
-
-	# create_test_users(usersDB)
-	# create_test_tasks(tasksDB)
-
-	a = usersDB.getLastID()
-	print(a)
-
-	a = usersDB.getUserByTgId("123")
-
-	a.addPoints(50)
-	a.fname = 'Eliot'
-	a.changeName('eliot')
-	a.changeStatus(21)
-	usersDB.updateUser(a)
-
-	# if a is not None:
-	# 	print(a.dumpToDict())
-	# else:
-	# 	print(a)
-
-	# a = usersDB.isExistsID(5)
-	# print(a)
-
-	# usersDB._updateValue(4, 'score', '50')
 
 	run_bot(usersDB, tasksDB, solutionsDB)
 
--- test_run.py
+++ test_run.py
@@ -1,6 +1,19 @@
 from utils import TasksDB, SolutionsDB, UsersDB
 
 
+def create_test_users(usersDB):
+	usersDB.createAndAddNewUser('123', 'qwerty')
+	usersDB.createAndAddNewUser('124')
+	usersDB.createAndAddNewUser('125', 'test')
+	usersDB.createAndAddNewUser('126', 'Steve')
+
+def create_test_tasks(tasksDB):
+	tasksDB.createAndAddNewTask("What is it?", "Linux", 1000)
+	tasksDB.createAndAddNewTask("Who is it?", "Tux", 700)
+	tasksDB.createAndAddNewTask("Where is it?", "Antarctica", 750)
+	tasksDB.createAndAddNewTask("When is it?", "Now", 5000)
+
+
 def main():
 	tasksDB = TasksDB()
 	tasksDB.start()
@@ -12,6 +25,30 @@ def main():
 
 	usersDB = UsersDB()
 	usersDB.start()
+
+	# create_test_users(usersDB)
+	# create_test_tasks(tasksDB)
+
+	a = usersDB.getLastID()
+	print(a)
+
+	a = usersDB.getUserByTgId("123")
+
+	a.addPoints(50)
+	a.fname = 'Eliot'
+	a.changeName('eliot')
+	a.changeStatus(21)
+	usersDB.updateUser(a)
+
+	# if a is not None:
+	# 	print(a.dumpToDict())
+	# else:
+	# 	print(a)
+
+	# a = usersDB.isExistsID(5)
+	# print(a)
+
+	# usersDB._updateValue(4, 'score', '50')
 
 	a = tasksDB.getTaskById(4)
 	u = usersDB.getUserById(6)
